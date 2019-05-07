4591461572cf718a072bc323d11dbbd97b02d747
noahshpak <noahshpak@gmail.com>
2019-05-07 07:57:34 -0400

    basic ngram language model

--- chapterthree/ngrams.py
+++ chapterthree/ngrams.py
@@ -11,14 +11,14 @@ class LM:
         self.nlp = spacy.load("en_core_web_sm")
 
     def tokenize(self, sentence: str) -> List[str]:
-        begin = [BEGIN_TOKEN] * self.n
-        end = [END_TOKEN] * self.n
+        begin = [BEGIN_TOKEN] * (self.n-1) if self.n > 1 else [BEGIN_TOKEN]
+        end = [END_TOKEN] * (self.n-1) if self.n > 1 else [END_TOKEN]
         return begin + [token.text.lower() for token in self.nlp(sentence)] + end
 
     def train(self, corpus: List[str]) -> Dict:
         tokens = [t for sentence in corpus for t in self.tokenize(sentence)]
         if self.n == 1:
-            return {t: count/len(tokens) for t,count in Counter(tokens).items()}
+            self.unigrams = {t: count/len(tokens) for t,count in Counter(tokens).items()}
         if self.n > len(tokens):
             raise ValueError(f"Corpus of length {len(tokens)} is not long enough for ngram of size {self.n}")
         ngrams, context_grams = defaultdict(int), defaultdict(int)
@@ -26,20 +26,22 @@ class LM:
             ngrams[tuple(tokens[i:i+self.n])] += 1
             context_grams[tuple(tokens[i:i+self.n-1])] += 1
         else:
-            context_grams[tuple(tokens[i+1:])] += 1
+            context_grams[tuple(tokens[i+self.n:])] += 1
 
         self.ngrams = ngrams
         self.context_grams = context_grams
 
     def proba(self, w: str, ctx: Tuple[str]):
-        ngram = tuple(list(ctx) + [w])
-        if not self.context_grams[ctx]: return 0
+        if self.n == 1:
+            return self.unigrams.get(w.lower(), 0.0) 
+        ngram = tuple([c.lower() for c in list(ctx)] + [w.lower()])
+        if not self.context_grams[ctx]: return 0.0
         return self.ngrams[ngram] / self.context_grams[ctx]
     
     def sequence_probability(self, sentence: str):
         tokens = self.tokenize(sentence)
-        prob = 1
+        prob = 1.0
         for i in range(len(tokens)-self.n):
             prob *= self.proba(tokens[i], tuple(tokens[i:i+self.n]))
         return prob
-
+        
--- tests/test_ngrams.py
+++ tests/test_ngrams.py
@@ -1,9 +1,15 @@
 import pytest
-from chapterthree import ngram
+from chapterthree import ngrams
 
 def test_unigrams():
-    corpus = [ "I am Sam", "Sam I am", "I am Sam", "I do not like green eggs and ham"]
-    
+    corpus = [ "I am Sam", "Sam I am", "I do not like green eggs and ham"]
+    lm = ngrams.LM(n=1)
+    lm.train(corpus)
+    probability = 1 / 10
+    assert lm.proba("Sam", ())  == probability
 
 def test_bigrams():
-    corpus = [ "I am Sam", "Sam I am", "I am Sam", "I do not like green eggs and ham"]
+    corpus = [ "I am Sam", "Sam I am", "I do not like green eggs and ham"]
+    lm = ngrams.LM(n=2)
+    lm.train(corpus)
+    assert lm.proba("I", ("<s>",)) == 2/3
