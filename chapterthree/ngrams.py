426d171e13ad94d5189d033faf45b34503e2c0ea
noahshpak <noahshpak@gmail.com>
2019-05-08 08:32:41 -0400

    Next most likely word

--- chapterthree/ngrams.py
+++ chapterthree/ngrams.py
@@ -1,4 +1,5 @@
 import spacy
+import numpy as np
 from typing import List, Dict, Tuple
 from collections import Counter, defaultdict
 
@@ -16,20 +17,21 @@ class LM:
         return begin + [token.text.lower() for token in self.nlp(sentence)] + end
 
     def train(self, corpus: List[str]) -> Dict:
-        tokens = [t for sentence in corpus for t in self.tokenize(sentence)]
+        self.tokens = [t for sentence in corpus for t in self.tokenize(sentence)]
+        self.vocab = list(set(self.tokens))
         if self.n == 1:
-            self.unigrams = {t: count/len(tokens) for t,count in Counter(tokens).items()}
-        if self.n > len(tokens):
-            raise ValueError(f"Corpus of length {len(tokens)} is not long enough for ngram of size {self.n}")
-        ngrams, context_grams = defaultdict(int), defaultdict(int)
-        for i in range(len(tokens)-self.n):
-            ngrams[tuple(tokens[i:i+self.n])] += 1
-            context_grams[tuple(tokens[i:i+self.n-1])] += 1
+            self.unigrams = {t: count/len(self.tokens) for t,count in Counter(self.tokens).items()}
+            return
+        
+        if self.n > len(self.tokens):
+            raise ValueError(f"Corpus of length {len(self.tokens)} is not long enough for ngram of size {self.n}")
+        
+        self.ngrams, self.context_grams = defaultdict(int), defaultdict(int)
+        for i in range(len(self.tokens)-self.n):
+            self.ngrams[tuple(self.tokens[i:i+self.n])] += 1
+            self.context_grams[tuple(self.tokens[i:i+self.n-1])] += 1
         else:
-            context_grams[tuple(tokens[i+self.n:])] += 1
-
-        self.ngrams = ngrams
-        self.context_grams = context_grams
+            self.context_grams[tuple(self.tokens[i+self.n:])] += 1
 
     def proba(self, w: str, ctx: Tuple[str]):
         if self.n == 1:
@@ -44,4 +46,12 @@ class LM:
         for i in range(len(tokens)-self.n):
             prob *= self.proba(tokens[i], tuple(tokens[i:i+self.n]))
         return prob
+
+    def next_most_likely(self, seed: Tuple[str]) -> Tuple[str, float]:
+        seed = tuple(t.lower() for t in seed)
+        next_word_space = np.array([self.proba(w, seed) for w in self.vocab])
+        most_likely_idx = np.argmax(next_word_space)
+        most_likely_prob = next_word_space[most_likely_idx]
+        same_prob = [(self.vocab[i], prob) for i, prob in enumerate(next_word_space) if prob == most_likely_prob]
+        return same_prob[np.random.randint(0, len(same_prob))]
         
--- tests/test_ngrams.py
+++ tests/test_ngrams.py
@@ -13,3 +13,6 @@ def test_bigrams():
     lm = ngrams.LM(n=2)
     lm.train(corpus)
     assert lm.proba("I", ("<s>",)) == 2/3
+    assert lm.next_most_likely(("I",)) == ("am", 2/3)
+    nml = lm.next_most_likely(("am",)) 
+    assert nml == ("sam", 1/2) or nml == ("</s>", 1/2)
